1b988aef955fe6f80fe95342fa7e223e6d344440
Martin Petit <martin@igloo.be>
2018-08-07 11:25:51 +0200

    fix boom problem after update + add a unit test for s3 storage invalidResponse

--- test/s3_test.js
+++ test/s3_test.js
@@ -2,17 +2,30 @@
 const chai = require('chai')
 chai.should()
 
+const { expect } = chai
+
 const request = require('supertest')
 
 const {requestFileToken} = require('./fixtures/utils')
 const appFactory = require('./fixtures/app')
 let app = null
 
+const S3Storage = require('../lib/amaging/storage/s3-storage')
+
 if (process.env.TEST_ENV !== 's3') {
   describe.skip('S3 Storage', S3StorageTest)
 } else {
   describe('S3 Storage', S3StorageTest)
 }
+
+describe('S3 Storage - unit test', () => {
+  it('S3Storage.InvalidResponse', () => {
+    const err = S3Storage.InvalidResponse('verb', { foo: true, statusCode: 123 })
+    expect(err.isBoom).to.equal(true)
+    expect(err.message).to.equal('Invalid VERB response from S3. (Status: 123)')
+    expect(err.output.statusCode).to.deep.equal(500)
+  })
+})
 
 function S3StorageTest () {
   /*

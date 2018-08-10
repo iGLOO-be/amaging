3bca54986691a58307dfed5257305bcfe8e5a93b
Martin Petit <martin@igloo.be>
2018-08-10 17:12:40 +0200

    Fix bug with chai

--- test/utils_test.js
+++ test/utils_test.js
@@ -6,6 +6,6 @@ chai.should()
 describe('utils.cleanAmagingFile', () =>
   test('Must clean ../', () => {
     const str = 'some/../path/../to/../../../../one/resource'
-    return cleanAmagingFile(str).should.equal('some/path/to/one/resource')
+    cleanAmagingFile(str).should.equal('some/path/to/one/resource')
   })
 )
